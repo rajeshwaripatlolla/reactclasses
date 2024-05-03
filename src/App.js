688f8ca0716039a93512d5d22dffec193366e2b4
rajeshwaripatlolla <rajeshwaripatlolla3@gmail.com>
2024-05-03 18:27:28 +0530

    proptask

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
 
 import './App.css';
-import Btn from './Components/function components/button';
+
+
+import Dummy from './Components/function components/dummy/prop';
 function App() {
-  const buttonArray =[
-    {text:"signUp",
-    width:100,
-    height:100,
-    bgColor:"green"},
-  
-    {text:"login",
-    width:100,
-    height:100,
-    bgColor:"blue"},
 
-    {text:"submit",
-    width:100,
-    height:100,
-    bgColor:"red"},
-
-    {text:"onClick",
-    width:100,
-    height:100,
-    bgColor:"orange"}
-    
-]
   return (
    <div>
-    {
-      buttonArray.map(eachbutton=>{
-        const {text,width, height ,bgColor }=eachbutton
-        return(
-         <Btn text= {text} bgColor={bgColor} buttonproperties={
-          {width:width,
-          height:height}
-         }/> 
-        )
-      })
-    }
-   {/* <Btn text = {"Login"} bgColor = {"blue"}  buttonproperties = {{
-    width:100,
-    height:100
-   }}
-  />
-   <Btn text= {"Signup"} bgColor= {"orange"} buttonproperties = {{
-    width:100,
-    height:100
-   }}/>
-   <Btn text = {"submit"} bgColor= {"green"} buttonproperties = {{
-    width:100,
-    height:100
-   }} /> */}
-
+  <Dummy/>
      </div>
      )}
 
---
+++ src/Components/function components/dummy/productCard.js
@@ -0,0 +1,32 @@
+
+  export const ProductCard = (props)=>{
+   const {src,price,title,rating , brand} = props
+    return(
+        <div style={
+            {
+              display:"flex",
+               flexDirection:"column",
+               justifyContent:"center",
+               alignItems:"center",
+               backgroundColor:"white",
+               margin:"15px",
+               height:'48vh',
+            padding:"20px",
+            width:'15%',
+            borderRadius:"10px"
+            }
+        }>
+
+        <img style={
+                {
+                    borderRadius:"10px"
+                }
+            }  src={src} alt="" height={200} width={200}/>
+        <h6>{title}</h6>
+        <h5>${price}</h5>
+        <h5>Rating: {rating}</h5>
+        <p>Brand:{brand} </p>
+
+        </div>
+    )
+}
